76940eea3691633328f4c19287742da28b8c5b0d
Yahia Hashem El-Tayeb <yahia.eltaib@yahoo.com>
2020-07-28 22:23:04 +0200

    feat: implement /dishes endpoint & Dish model

--- app.py
+++ app.py
@@ -2,12 +2,43 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import Dish
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     CORS(app)
+
+    @app.route('/dishes', methods=['GET'])
+    def retrieve_dishes():
+        try:
+            dishes = [ dish.format() for dish in Dish.query.all()]
+            return jsonify({
+                'success': True,
+                'dishes': dishes
+            })
+        except:
+            abort(422)
+
+
+    # Error handlers
+    
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            'success': False,
+            'error': 404,
+            'message': 'Resource Not Found'
+        }), 404
+
+    @app.errorhandler(422)
+    def not_processable(error):
+        return jsonify({
+            'success': False,
+            'error': 422,
+            'message': 'Not Processable'
+        }), 422
 
     return app
 
---
+++ models.py
@@ -0,0 +1,80 @@
+import os
+from sqlalchemy import (
+    Column, String, Integer, create_engine, ForeignKey,
+    Float, Boolean
+)
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import json
+
+database_path = os.environ['DATABASE_URL']
+
+db = SQLAlchemy()
+
+
+def rollback():
+    """Rollback if transaction not successful"""
+    db.session.rollback()
+
+
+def close_connection():
+    """Close the connection to make it available for reusing"""
+    db.session.close()
+
+
+def setup_db(app, database_path=database_path):
+    """
+    setup_db(app)
+        binds a flask application and a SQLAlchemy service
+    """
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
+
+class Dish(db.Model):
+    """
+    Dish
+    """
+    __tablename__ = 'dishes'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    image = Column(String)
+    category = Column(String)
+    label = Column(String)
+    price = Column(Float)
+    featured = Column(Boolean)
+    description = Column(String)
+
+    def __init__(self, name, category, label, price, featured, description):
+        self.name = name
+        self.category = category
+        self.label = label
+        self.price = price
+        self.featured = featured
+        self.description = description
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'category': self.category,
+            'label': self.label,
+            'price': self.price,
+            'featured': self.featured,
+            'description': self.description
+        }
