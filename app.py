090f3335d01ce7886363044dd133902d4e890be2
Yahia Hashem El-Tayeb <yahia.eltaib@yahoo.com>
2020-08-01 12:18:26 +0200

    feat: implement rest of /dishes methods (POST, PATCH, DELETE)

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from models import Dish, setup_db
+from models import Dish, setup_db, rollback, close_connection
 import sys
 
 
@@ -25,7 +25,97 @@ def create_app(test_config=None):
             abort(422)
 
 
-    # Error handlers
+    @app.route('/dishes', methods=['POST'])
+    def create_dish():
+        body = request.get_json()
+
+        # if body not exist, will be considered bad request
+        if not body:
+            abort(400)
+
+        # if any of not null args not exist in the body it will be considered a bad request
+        for arg in ['name', 'image', 'category', 'price']:
+            if arg not in body:
+                abort(400)
+        
+        # extract args
+        name = body.get('name', None)
+        image = body.get('image', None)
+        category = body.get('category', None)
+        price = body.get('price', None)
+
+        try:
+            dish = Dish(name=name, image=image, category=category, price=price)
+            dish.insert()
+            dish_id = dish.id
+            dishes = [dish.format() for dish in Dish.query.all()]
+            return jsonify({
+                'success': True,
+                'created': dish_id,
+                'dishes': dishes
+            })
+
+        except:
+            print(sys.exc_info())
+            rollback()
+            abort(422)
+        finally:
+            close_connection()
+
+    @app.route('/dishes/<int:dish_id>', methods=['PATCH'])
+    def update_dish_price(dish_id):
+        dish = Dish.query.filter(Dish.id == dish_id).one_or_none()
+
+        # if there is no dish exist for dish_id, Will be consider 404 error
+        if not dish: abort(404)
+
+        body = request.get_json()
+
+        # if body not exist or not containg price, Will be considered bad request
+        if not body or 'price' not in body:
+            abort(400)
+
+        # extract args
+        price = body.get('price', None)
+
+        try:
+            dish.price = price
+            dish.update()
+            formatted_dish = dish.format()
+            return jsonify({
+                'success': True,
+                'dish': formatted_dish
+            })
+        except:
+            print(sys.exc_info)
+            rollback()
+            abort(422)
+        finally:
+            close_connection()
+
+    @app.route('/dishes/<int:dish_id>', methods=['DELETE'])
+    def remove_dish(dish_id):
+        dish = Dish.query.filter(Dish.id == dish_id).one_or_none()
+
+        # if dish with the given dish_id, Will be consider unprocessable request
+        if not dish: abort(422)
+
+        try:
+            dish.delete()
+            dish_id = dish.id
+            return jsonify({
+                'success': True,
+                'deleted': dish_id,
+                'dishes': [dish.format() for dish in Dish.query.all()]
+            })
+        except:
+            print(sys.exc_info())
+            rollback()
+            abort(422)
+        finally:
+            close_connection()
+
+    # ======Error handlers==========
     
     @app.errorhandler(404)
     def not_found(error):
@@ -42,6 +132,22 @@ def create_app(test_config=None):
             'error': 422,
             'message': 'Not Processable'
         }), 422
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            'success': False,
+            'error': 400,
+            'message': 'Bad Request'
+        }), 400
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return jsonify({
+            'success': False,
+            'error': 500,
+            'message': 'Internal Server Error'
+        }), 500
 
     return app
 
