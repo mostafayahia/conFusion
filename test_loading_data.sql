664b343a6eada06e667435962485bfb37b1ce00b
Yahia Hashem El-Tayeb <yahia.eltaib@yahoo.com>
2020-08-01 12:20:57 +0200

    test: add insert sql statements for confustion & confusion_test DBs

--- test_loading_data.sql
+++ test_loading_data.sql
@@ -1,11 +1,18 @@
--- DISHES
--- ########
+-- Dishes
+-- #################
 
-DELETE FROM dishes;
-
+DROP TABLE IF EXISTS dishes;
+CREATE TABLE dishes (
+  id serial primary key,
+  name varchar,
+  image varchar,
+  category varchar,
+  label varchar,
+  price float,
+  featured boolean,
+  description varchar
+);
 INSERT INTO dishes (name, image, category, label, price, featured,  description) VALUES ('Uthappizza', 'images/uthappizza.png', 'mains', 'Hot', 4.99, 'True',  'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.');
 INSERT INTO dishes (name, image, category, label, price, featured,  description) VALUES ('Zucchipakoda', 'images/zucchipakoda.png', 'appetizer', '', 1.99, 'False',  'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce');
 INSERT INTO dishes (name, image, category, label, price, featured,  description) VALUES ('Vadonut', 'images/vadonut.png', 'appetizer', 'New', 1.99, 'False',  'A quintessential ConFusion experience, is it a vada or is it a donut?');
 INSERT INTO dishes (name, image, category, label, price, featured,  description) VALUES ('ElaiCheese Cake', 'images/elaicheesecake.png', 'dessert', '', 2.99, 'False',  'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms');
-
-
