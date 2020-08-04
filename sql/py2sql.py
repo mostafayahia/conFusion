95f12c131c4f03da876644c0d3ebdf95608ff593
Yahia Hashem El-Tayeb <yahia.eltaib@yahoo.com>
2020-08-04 17:06:00 +0200

    chore: move related sql files to sql dir

--- sql/py2sql.py
+++ sql/py2sql.py
@@ -283,7 +283,7 @@ def is_float(s):
     return re.match(r'^-?\d+(?:\.\d+)?$', str(s)) is not None
 
 # setting table name
-table_name = 'dishes'
+table_name = 'promotions'
 
 # setting table cols
 table_cols = []
--- sql/test_loading_data.sql
+++ sql/test_loading_data.sql
@@ -16,3 +16,17 @@ INSERT INTO dishes (name, image, category, label, price, featured,  description)
 INSERT INTO dishes (name, image, category, label, price, featured,  description) VALUES ('Zucchipakoda', 'images/zucchipakoda.png', 'appetizer', '', 1.99, 'False',  'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce');
 INSERT INTO dishes (name, image, category, label, price, featured,  description) VALUES ('Vadonut', 'images/vadonut.png', 'appetizer', 'New', 1.99, 'False',  'A quintessential ConFusion experience, is it a vada or is it a donut?');
 INSERT INTO dishes (name, image, category, label, price, featured,  description) VALUES ('ElaiCheese Cake', 'images/elaicheesecake.png', 'dessert', '', 2.99, 'False',  'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms');
+
+-- Promotions
+-- #################
+DROP TABLE IF EXISTS promotions;
+CREATE TABLE promotions (
+  id serial primary key,
+  name varchar,
+  image varchar,
+  label varchar,
+  price float,
+  featured boolean,
+  description varchar
+);
+INSERT INTO promotions (name, image, label, price, featured,  description) VALUES ('Weekend Grand Buffet', 'images/buffet.png', 'New', 19.99, 'True',  'Featuring mouthwatering combinations with a choice of five different salads, six enticing appetizers, six main entrees and five choicest desserts. Free flowing bubbly and soft drinks. All for just $19.99 per person ');
